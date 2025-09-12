b0387a4f1d7bb4026150c1a8cd3aac2024cb025d
Akshay <akshayjalmane999@gmail.com>
2025-09-12 07:06:24 +0530

    gpio initial setup for driver developement setting up stm32f401xx.h

---
+++ STM32CubeIDE/workspace/stm32f4xx_drivers/drivers/inc/stm32f401xx.h
@@ -0,0 +1,21 @@
+/*
+ * stm32f401xx.h
+ *
+ *  Created on: Sep 11, 2025
+ *      Author: aksha
+ */
+
+#ifndef INC_STM32F401XX_H_
+#define INC_STM32F401XX_H_
+/*
+Base addrress of Falsh and Sram memory
+*/
+#define FLASH_BASEADDR	0x08000000U		//reference manual page 45
+#define SRAM_BASEADDR	0x20000000U		//reference manual page 42
+#define ROM_BASEADDR	0x1FFF0000U		//reference manual page 45 rom is also called system memory
+
+
+
+
+
+#endif /* INC_STM32F401XX_H_ */
