35daca198e7e25c9494acae3cff3573fc2649403
Akshay <akshayjalmane999@gmail.com>
2025-09-25 22:16:35 +0530

    improved functionality for gpio
    -- led toggle
    -- led and push button

--- STM32CubeIDE/workspace/stm32f4xx_drivers/Debug/Src/subdir.mk
+++ STM32CubeIDE/workspace/stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/001ledToggle.c \
+../Src/002led_button.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/001ledToggle.o \
+./Src/002led_button.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/001ledToggle.d \
+./Src/002led_button.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/001ledToggle.cyclo ./Src/001ledToggle.d ./Src/001ledToggle.o ./Src/001ledToggle.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/002led_button.cyclo ./Src/002led_button.d ./Src/002led_button.o ./Src/002led_button.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
